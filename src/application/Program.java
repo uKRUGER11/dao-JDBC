9150538668da86f4790494d00df3d32ce61e539b
Gabriel Kruger <krugergabriel27@outlook.com>
2022-03-24 21:15:10 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findyById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 	}
 }
